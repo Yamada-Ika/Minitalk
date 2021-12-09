c912bae07061d2eeb8841f7b723c002189a8f7d7
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-12-09 13:48:50 +0900

    Fix server.c

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 09:26:13 by iyamada           #+#    #+#             */
-/*   Updated: 2021/12/09 13:32:26 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/12/09 13:46:58 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,11 @@ void	ft_init_receive_info(t_receive_info *rec_info, int flag)
 		rec_info->is_str_len_sent = 1;
 		rec_info->bit_count = 0;
 		rec_info->decimal_num = 0;
-		return ;
 	}
 	if (flag == 1)
 	{
 		rec_info->bit_count = 0;
 		rec_info->decimal_num = 0;
-		return ;
 	}
 	if (flag == 2)
 	{
@@ -50,18 +48,19 @@ void	ft_print_received_str(char *s)
 	free(s);
 }
 
-void	ft_receive_str_malloc(char **str, int str_len)
+bool	ft_receive_str_malloc(char **str, int str_len)
 {
 	*str = (char *)malloc((str_len + 1) * sizeof(char));
 	if (*str == NULL)
 	{
 		write(STDERR_FILENO, "Failed to memory allocate!\n", 28);
-		return ;
+		return (false);
 	}
 	(*str)[str_len] = '\0';
+	return (true);
 }
 
-void	sig_handler(int	signal)
+int	sig_handler(int	signal)
 {
 	static t_receive_info	receive;
 	static char				*receive_str;
@@ -70,9 +69,8 @@ void	sig_handler(int	signal)
 	receive.bit_count++;
 	if (receive.is_str_len_sent == 0 && receive.bit_count == sizeof(int) * BYTE)
 	{
-		ft_receive_str_malloc(&receive_str, receive.decimal_num);
-		if (receive_str == NULL)
-			return ;
+		if (ft_receive_str_malloc(&receive_str, receive.decimal_num) == false)
+			return (1);
 		ft_init_receive_info(&receive, 0);
 	}
 	else if (receive.is_str_len_sent == 1 && receive.bit_count == BYTE)
@@ -81,7 +79,7 @@ void	sig_handler(int	signal)
 		{
 			ft_print_received_str(receive_str);
 			ft_init_receive_info(&receive, 2);
-			return ;
+			return (0);
 		}
 		receive_str[receive.str_index] = (char)(receive.decimal_num);
 		receive.str_index++;
