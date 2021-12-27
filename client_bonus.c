5cdb64d319e2c881178b955155c23a4824c8dedb
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-12-28 01:50:00 +0900

    [Fix]

--- client_bonus.c
+++ client_bonus.c
@@ -6,112 +6,92 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 09:26:11 by iyamada           #+#    #+#             */
-/*   Updated: 2021/12/25 01:45:35 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/12/28 01:49:15 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_minitalk.h"
-#include <stdio.h>
+#include "ft_minitalk_bonus.h"
+
+volatile sig_atomic_t	g_sig;
 
 void	print_receive_signal(int signal)
 {
-	// if (signal == SIGUSR1)
-	// 	write(STDOUT_FILENO, "SIGUSR1 received!\n", 19);
-	// if (signal == SIGUSR2)
-	// 	write(STDOUT_FILENO, "SIGUSR2 received!\n", 19);
-	// write(STDOUT_FILENO, "called!\n", 9);
+	g_sig = signal;
 }
 
-int	ft_kill(pid_t pid, int signal, int num)
+int	ft_kill(pid_t pid, int signal)
 {
-	// if (signal == SIGUSR1)
-	// 	write(STDOUT_FILENO, "SIGUSR1 sent!\n", 15);
-	// if (signal == SIGUSR2)
-	// 	write(STDOUT_FILENO, "SIGUSR2 sent!\n", 15);
+	if (signal == SIGUSR1)
+		ft_putstr_fd("SIGUSR1 sent!\n", STDOUT_FILENO);
+	if (signal == SIGUSR2)
+		ft_putstr_fd("SIGUSR2 sent!\n", STDOUT_FILENO);
 	return (kill(pid, signal));
 }
 
-int	ft_send_data_to_pid(pid_t pid, int data, int size)
+static void	ft_send_data(pid_t pid, size_t data, unsigned long size)
 {
-	int	sig;
-	int	bit;
-	int	j;
+	int		signal;
+	size_t	bit;
+	size_t	j;
 
 	j = 0;
 	while (j < size)
 	{
-		bit = (data >> j) & 0b1;
+		bit = (data >> j) & 1;
 		if (bit == 0)
-			sig = SIGUSR1;
+			signal = SIGUSR1;
 		if (bit == 1)
-			sig = SIGUSR2;
-		if (ft_kill(pid, sig, j) == KILL_FAILE)
+			signal = SIGUSR2;
+		if (ft_kill(pid, signal) == KILL_FAILE)
 		{
-			write(STDERR_FILENO, "Failed to send!\n", 16);
-			return (SEND_FAILE);
+			ft_putstr_fd("Failed to send!\n", STDERR_FILENO);
+			exit(SEND_ERROR);
 		}
 		pause();
-		// pause();
-		// usleep(5000);
+		usleep(100);
 		j++;
 	}
-	return (SEND_SUCCESS);
 }
 
-int	ft_send_str_to_process(pid_t pid, char *str)
+static void	ft_send_str(pid_t pid, char *str)
 {
-	size_t	i;
 	size_t	str_len;
-	char	c;
+	size_t	i;
 
 	if (str == NULL)
-		return (-1);
+		return ;
 	i = 0;
 	str_len = strlen(str);
-	if (ft_send_data_to_pid(pid, (int)str_len, sizeof(int) * BYTE) == -1)
-		return (SEND_FAILE);
+	ft_send_data(pid, str_len, sizeof(size_t) * BYTE);
 	while (i < str_len)
 	{
-		c = str[i];
-		if (ft_send_data_to_pid(pid, c, BYTE) == SEND_FAILE)
-			return (SEND_FAILE);
+		ft_send_data(pid, str[i], BYTE);
 		i++;
 	}
-	if (ft_send_data_to_pid(pid, EOT, BYTE) == SEND_FAILE)
-		return (SEND_FAILE);
-	return (SEND_SUCCESS);
+	ft_send_data(pid, EOT, BYTE);
 }
-
-pid_t	ft_get_pid(char *s)
+static pid_t	ft_get_pid(char *s)
 {
 	pid_t	pid;
 	char	*end;
 
-	pid = (pid_t)strtoll(s, &end, 10);
-	if (strcmp(end, ""))
-		return (ERROR_PID);
-	if (pid <= PID_MIN)
-		return (ERROR_PID);
+	pid = (pid_t)ft_strtoll(s, &end, 10);
+	if (strcmp(end, "") != 0 || pid <= PID_MIN)
+	{
+		ft_putstr_fd("Invalid PID!\n", STDERR_FILENO);
+		exit(ARG_ERROR);
+	}
 	return (pid);
 }
 
 int	main(int argc, char *argv[])
 {
-	pid_t	server_pid;
-
 	signal(SIGUSR1, print_receive_signal);
 	signal(SIGUSR2, print_receive_signal);
-	if (argc != 3 || argv == NULL)
+	if (argc != 3)
 	{
-		write(STDERR_FILENO, "Invalid argument!\n", 18);
-		return (1);
+		ft_putstr_fd("Invalid argument!\n", STDERR_FILENO);
+		return (ARG_ERROR);
 	}
-	server_pid = ft_get_pid(argv[1]);
-	if (server_pid == ERROR_PID)
-	{
-		write(STDERR_FILENO, "Invalid PID!\n", 13);
-		return (1);
-	}
-	if (ft_send_str_to_process(server_pid, argv[2]) == SEND_FAILE)
-		return (SEND_FAILE);
+	ft_send_str(ft_get_pid(argv[1]), argv[2]);
 }
--- server_bonus.c
+++ server_bonus.c
@@ -6,76 +6,61 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 09:26:13 by iyamada           #+#    #+#             */
-/*   Updated: 2021/12/27 14:57:05 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/12/28 01:31:02 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_minitalk.h"
-#include "ft_receive_info.h"
-#include <stdio.h>
+#include "ft_minitalk_bonus.h"
 
-static t_receive_info	rec;
+volatile sig_atomic_t	g_sig;
 
-void	ft_init_receive_info(int flag)
+static void	ft_init_receive_info(t_receive_info *rec, int flag)
 {
-	if (flag == 0)
+	rec->bit_cnt = 0;
+	rec->decimal = 0;
+	if (flag == STR_LEN_SENT)
+		rec->is_len_sent = true;
+	if (flag == STR_SENT)
 	{
-		rec.is_len_sent = true;
-		rec.bit_cnt = 0;
-		rec.decimal = 0;
-	}
-	if (flag == 1)
-	{
-		rec.bit_cnt = 0;
-		rec.decimal = 0;
-	}
-	if (flag == 2)
-	{
-		rec.is_len_sent = false;
-		rec.bit_cnt = 0;
-		rec.decimal = 0;
-		rec.index = 0;
+		rec->is_len_sent = false;
+		rec->index = 0;
 	}
 }
 
-void	ft_print_str(void)
+static void	ft_print_str(t_receive_info *rec)
 {
-	int	str_len;
-
-	str_len = (int)strlen(rec.str);
-	write(STDOUT_FILENO, rec.str, str_len + 1);
-	putc('\n', stdout);
-	free(rec.str);
+	ft_putstr(rec->str);
+	ft_putchar('\n');
+	free(rec->str);
+	ft_init_receive_info(rec, STR_SENT);
 }
 
-bool	ft_allocate_for_str(void)
+static void	ft_allocate_for_str(t_receive_info *rec)
 {
 	int	str_len;
 
-	str_len = rec.decimal;
-	rec.str = (char *)malloc((str_len + 1) * sizeof(char));
-	if (rec.str == NULL)
+	str_len = rec->decimal;
+	rec->str = (char *)malloc((str_len + 1) * sizeof(char));
+	if (rec->str == NULL)
 	{
-		write(STDERR_FILENO, "Failed to memory allocate!\n", 28);
-		return (false);
+		ft_putstr_fd("Failed to memory allocate!\n", STDERR_FILENO);
+		exit(MEM_ERROR);
 	}
-	rec.str[str_len] = '\0';
-	return (true);
+	rec->str[str_len] = '\0';
+	ft_init_receive_info(rec, STR_LEN_SENT);
 }
 
 void	sig_handler(int sig, siginfo_t *info, void *ucontext)
 {
-	rec.decimal += (sig - SIGUSR1) << rec.bit_cnt;
-	rec.bit_cnt++;
-	rec.client_pid = info->si_pid;
-	rec.signal = sig;
-	// write(STDOUT_FILENO, "called!\n", 9);
-	// kill(info->si_pid, sig);
+	g_sig = sig;
+	ucontext = NULL;
+	kill(info->si_pid, sig);
 }
 
 int	main(void)
 {
-	struct sigaction	act;
+	struct sigaction		act;
+	static t_receive_info	rec;
 
 	printf("PID : %d\n", getpid());
 	sigemptyset(&act.sa_mask);
@@ -85,34 +70,22 @@ int	main(void)
 	sigaction(SIGUSR2, &act, NULL);
 	while (true)
 	{
-		// write(STDOUT_FILENO, "paused!\n", 9);
 		pause();
-		// write(STDOUT_FILENO, "started!\n", 9);
-		if (!rec.is_len_sent && rec.bit_cnt == sizeof(int) * BYTE)
-		{
-			if (!ft_allocate_for_str())
-				exit(1);
-			ft_init_receive_info(0);
-		}
+		rec.decimal += (g_sig - SIGUSR1) << rec.bit_cnt;
+		rec.bit_cnt++;
+		if (!rec.is_len_sent && rec.bit_cnt == sizeof(size_t) * BYTE)
+			ft_allocate_for_str(&rec);
 		else if (rec.is_len_sent && rec.bit_cnt == BYTE)
 		{
 			if (rec.decimal == EOT)
 			{
-				ft_print_str();
-				ft_init_receive_info(2);
-				// usleep(1000);
-				kill(rec.client_pid, rec.signal);
+				ft_print_str(&rec);
 				continue ;
 			}
 			rec.str[rec.index] = (char)(rec.decimal);
 			rec.index++;
-			// write(STDOUT_FILENO, &(rec.decimal), 1);
-			ft_init_receive_info(1);
+			ft_init_receive_info(&rec, CHAR_SENT);
 		}
-		// write(STDOUT_FILENO, "ready?...\n", 11);
-		// usleep(1000);
-		kill(rec.client_pid, rec.signal);
-		// write(STDOUT_FILENO, "sent!\n", 7);
 	}
 }
 
